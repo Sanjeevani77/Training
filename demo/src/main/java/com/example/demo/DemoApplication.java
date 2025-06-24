6e189a530085206d5f1d556613219745d13ec4c5
Sanjeevani77 <saichandrala1998@gmail.com>
2025-06-24 11:10:47 -0400

    Update DemoApplication.java

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemoApplication {
-	//test comment to verify the push and pull
+	//test comment to verify 1 the push and pull
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
